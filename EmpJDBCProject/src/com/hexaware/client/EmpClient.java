62a4476fd87507b0d3e30b5d1c03d6fb1aca9896
govind <govind.corp.trainer@gmail.com>
2025-04-08 12:34:00 +0530

    resolved conflict of Empclient.java

--- EmpJDBCProject/src/com/hexaware/client/EmpClient.java
+++ EmpJDBCProject/src/com/hexaware/client/EmpClient.java
@@ -4,11 +4,16 @@ public class EmpClient {
 	
  public static void main (String[] args) {
 	 System.out.println("Welcome");
-	 display();
+     display();
+     viewResults();
  }
  
  public static void display() {
 	 System.out.println("Display");
+	
  }
  
+ public static void viewResults() {
+	 System.out.println("view results");
+ }
 }
